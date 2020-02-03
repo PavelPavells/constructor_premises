ca5286e83d217977416cdb90e952698b0015a432
PavelPavells <hismailforyou@gmail.com>
2020-02-03 12:08:42 +0300

    [03.02.2020 - Constructor Premises; Pavel Smirnov]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import store from './store';
+import './App.scss';
 
-function App() {
+let App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/dashboard' component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
+    </Provider>
   );
 }
-
 export default App;
